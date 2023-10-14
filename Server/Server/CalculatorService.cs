be4f8b35f6023cddc075a63a6018af7cce2e8799
Dušan <duci.sudjic@gmail.com>
2023-10-14 16:11:33 +0200

    initial comit

---
+++ Server/Server/CalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public int GetMultiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int GetSum(int a, int b)
+        {
+            return a+ b;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService), new Uri("http://localhost:9002/CalculatorService")))
+            {
+                host.Open();
+                Console.WriteLine("Service is running. Press Enter to stop.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
